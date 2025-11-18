bd5e60b7010ed010aaf1d7b55a28673187d63e87
Krzysztof Taborowski <krzysztof.taborowski@nordicsemi.no>
2025-11-18 14:16:51 +0100

    bluetooth: lbs: add pairing mode and accept list support
    Connect only with bonded devices
    Add button actions:
    - unbound all
    - pair new device
    
    Signed-off-by: Krzysztof Taborowski <krzysztof.taborowski@nordicsemi.no>

--- samples/bluetooth/peripheral_lbs/src/main.c
+++ samples/bluetooth/peripheral_lbs/src/main.c
@@ -26,17 +26,26 @@
 
 #include <dk_buttons_and_leds.h>
 
-#define DEVICE_NAME             CONFIG_BT_DEVICE_NAME
-#define DEVICE_NAME_LEN         (sizeof(DEVICE_NAME) - 1)
+#define DEVICE_NAME	CONFIG_BT_DEVICE_NAME
+#define DEVICE_NAME_LEN (sizeof(DEVICE_NAME) - 1)
 
+#define RUN_STATUS_LED	       DK_LED1
+#define CON_STATUS_LED	       DK_LED2
+#define RUN_LED_BLINK_INTERVAL 1000
 
-#define RUN_STATUS_LED          DK_LED1
-#define CON_STATUS_LED          DK_LED2
-#define RUN_LED_BLINK_INTERVAL  1000
+#define USER_LED DK_LED3
 
-#define USER_LED                DK_LED3
+#define USER_BUTTON DK_BTN1_MSK
+#ifdef CONFIG_BT_LBS_PAIRING_MODE
+#define BOND_DELETE_BUTTON DK_BTN3_MSK
+#define PAIRING_BUTTON	   DK_BTN4_MSK
 
-#define USER_BUTTON             DK_BTN1_MSK
+#define BT_LE_ADV_CONN_ACCEPT_LIST                                                                 \
+	BT_LE_ADV_PARAM(BT_LE_ADV_OPT_CONN | BT_LE_ADV_OPT_FILTER_CONN, BT_GAP_ADV_FAST_INT_MIN_2, \
+			BT_GAP_ADV_FAST_INT_MAX_2, NULL)
+
+static bool pairing_mode = false;
+#endif /* CONFIG_BT_LBS_PAIRING_MODE */
 
 static bool app_button_state;
 static struct k_work adv_work;
@@ -50,10 +59,62 @@ static const struct bt_data sd[] = {
 	BT_DATA_BYTES(BT_DATA_UUID128_ALL, BT_UUID_LBS_VAL),
 };
 
+#ifdef CONFIG_BT_LBS_PAIRING_MODE
+static void setup_accept_list_cb(const struct bt_bond_info *info, void *user_data)
+{
+	int *bond_cnt = user_data;
+	if ((*bond_cnt) < 0) {
+		return;
+	}
+	int err = bt_le_filter_accept_list_add(&info->addr);
+	printk("Added following peer to whitelist: %x %x \n", info->addr.a.val[0],
+	       info->addr.a.val[1]);
+	if (err) {
+		printk("Cannot add peer to Filter Accept List (err: %d)\n", err);
+		(*bond_cnt) = -EIO;
+	} else {
+		(*bond_cnt)++;
+	}
+}
+#endif /* CONFIG_BT_LBS_PAIRING_MODE */
+
 static void adv_work_handler(struct k_work *work)
 {
-	int err = bt_le_adv_start(BT_LE_ADV_CONN_FAST_2, ad, ARRAY_SIZE(ad), sd, ARRAY_SIZE(sd));
+	int err = 0;
+	struct bt_le_adv_param adv_param = *BT_LE_ADV_CONN_FAST_2;
+#ifdef CONFIG_BT_LBS_PAIRING_MODE
+	do {
+		int allowed_cnt = 0;
 
+		err = bt_le_filter_accept_list_clear();
+		if (err) {
+			printk("Cannot clear Filter Accept List (err: %d)\n", err);
+			break;
+		}
+
+		if (pairing_mode) {
+			printk("Pairing mode, advertising without Accept list\n");
+			pairing_mode = false;
+			break;
+		}
+
+		bt_foreach_bond(BT_ID_DEFAULT, setup_accept_list_cb, &allowed_cnt);
+		if (allowed_cnt < 0) {
+			printk("Acceptlist setup failed (err:%d)\n", allowed_cnt);
+			break;
+		}
+		if (allowed_cnt == 0) {
+			printk("No bonds found, advertising without Accept list\n");
+			break;
+		}
+		if (allowed_cnt > 0) {
+			printk("Advertising with Accept list \n with %d devices\n", allowed_cnt);
+			adv_param = *BT_LE_ADV_CONN_ACCEPT_LIST;
+		}
+	} while (0);
+#endif /* CONFIG_BT_LBS_PAIRING_MODE */
+
+	err = bt_le_adv_start(&adv_param, ad, ARRAY_SIZE(ad), sd, ARRAY_SIZE(sd));
 	if (err) {
 		printk("Advertising failed to start (err %d)\n", err);
 		return;
@@ -69,21 +130,35 @@ static void advertising_start(void)
 
 static void connected(struct bt_conn *conn, uint8_t err)
 {
+	int rc = 0;
 	if (err) {
 		printk("Connection failed, err 0x%02x %s\n", err, bt_hci_err_to_str(err));
 		return;
 	}
+#ifdef CONFIG_BT_LBS_PAIRING_MODE
+	rc = bt_conn_set_security(conn, BT_SECURITY_L4);
+	if (rc) {
+		printk("Failed to set security (err: %d)\n", rc);
+	}
+#endif /* CONFIG_BT_LBS_PAIRING_MODE */
 
 	printk("Connected\n");
 
-	dk_set_led_on(CON_STATUS_LED);
+	rc = dk_set_led_on(CON_STATUS_LED);
+	if (rc) {
+		printk("Failed to set LED (err: %d)\n", rc);
+	}
 }
 
 static void disconnected(struct bt_conn *conn, uint8_t reason)
 {
+	int rc = 0;
 	printk("Disconnected, reason 0x%02x %s\n", reason, bt_hci_err_to_str(reason));
 
-	dk_set_led_off(CON_STATUS_LED);
+	rc = dk_set_led_off(CON_STATUS_LED);
+	if (rc) {
+		printk("Failed to set LED (err: %d)\n", rc);
+	}
 }
 
 static void recycled_cb(void)
@@ -93,8 +168,7 @@ static void recycled_cb(void)
 }
 
 #ifdef CONFIG_BT_LBS_SECURITY_ENABLED
-static void security_changed(struct bt_conn *conn, bt_security_t level,
-			     enum bt_security_err err)
+static void security_changed(struct bt_conn *conn, bt_security_t level, enum bt_security_err err)
 {
 	char addr[BT_ADDR_LE_STR_LEN];
 
@@ -110,9 +184,9 @@ static void security_changed(struct bt_conn *conn, bt_security_t level,
 #endif
 
 BT_CONN_CB_DEFINE(conn_callbacks) = {
-	.connected        = connected,
-	.disconnected     = disconnected,
-	.recycled         = recycled_cb,
+	.connected = connected,
+	.disconnected = disconnected,
+	.recycled = recycled_cb,
 #ifdef CONFIG_BT_LBS_SECURITY_ENABLED
 	.security_changed = security_changed,
 #endif
@@ -161,10 +235,8 @@ static struct bt_conn_auth_cb conn_auth_callbacks = {
 	.cancel = auth_cancel,
 };
 
-static struct bt_conn_auth_info_cb conn_auth_info_callbacks = {
-	.pairing_complete = pairing_complete,
-	.pairing_failed = pairing_failed
-};
+static struct bt_conn_auth_info_cb conn_auth_info_callbacks = {.pairing_complete = pairing_complete,
+							       .pairing_failed = pairing_failed};
 #else
 static struct bt_conn_auth_cb conn_auth_callbacks;
 static struct bt_conn_auth_info_cb conn_auth_info_callbacks;
@@ -181,7 +253,7 @@ static bool app_button_cb(void)
 }
 
 static struct bt_lbs_cb lbs_callbacs = {
-	.led_cb    = app_led_cb,
+	.led_cb = app_led_cb,
 	.button_cb = app_button_cb,
 };
 
@@ -193,6 +265,29 @@ static void button_changed(uint32_t button_state, uint32_t has_changed)
 		bt_lbs_send_button_state(user_button_state);
 		app_button_state = user_button_state ? true : false;
 	}
+#ifdef CONFIG_BT_LBS_PAIRING_MODE
+	if (has_changed & BOND_DELETE_BUTTON) {
+		uint32_t bond_delete_button_state = button_state & BOND_DELETE_BUTTON;
+		if (bond_delete_button_state == 0) {
+			int err = bt_unpair(BT_ID_DEFAULT, BT_ADDR_LE_ANY);
+			if (err) {
+				printk("Cannot delete bond (err: %d)\n", err);
+			} else {
+				printk("Bond deleted successfully \n");
+			}
+		}
+	}
+	if (has_changed & PAIRING_BUTTON) {
+		uint32_t pairing_button_state = button_state & PAIRING_BUTTON;
+		if (pairing_button_state == 0) {
+			pairing_mode = true;
+			int err = bt_le_adv_stop();
+			if (err) {
+				printk("Cannot stop advertising err= %d \n", err);
+			}
+		}
+	}
+#endif /* CONFIG_BT_LBS_PAIRING_MODE */
 }
 
 static int init_button(void)
